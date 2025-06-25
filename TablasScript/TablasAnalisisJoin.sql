88f2473c56693e6b12b4d841880be6dd694cc9a0
davidethc <davidecondet@gmail.com>
2025-06-24 23:23:19 -0500

    01

---
+++ TablasScript/TablasAnalisisJoin.sql
@@ -0,0 +1,27 @@
+create table TablasAnalisisJoin
+(
+    qr_code           int  null,
+    id                int  null,
+    fecha             text null,
+    det               text null,
+    nombre_docente    text null,
+    nombre_asignatura text null,
+    result_id         text null,
+    feliz             text null,
+    interesado        text null,
+    motivado          text null,
+    entusiasmado      text null,
+    preocupado        text null,
+    temeroso          text null,
+    triste            text null,
+    cansado           text null,
+    UIDE              text null,
+    numrandom         text null,
+    rn                text null
+);
+INSERT INTO FinalProyecto.TablasAnalisisJoin(qr_code, id, fecha, det, nombre_docente, nombre_asignatura, result_id, feliz, interesado, motivado, entusiasmado, preocupado, temeroso, triste, cansado, UIDE, numrandom, rn) VALUES (387249, 13687, '2025-01-28 20:54:26', 'null', 'Fredy rolando alvarado gonzalez', 'Los procesos penales y recursos', '13687', '1', '0', '0', '0', '0', '0', '0', '0', 'null', '387249', '1');
+INSERT INTO FinalProyecto.TablasAnalisisJoin (qr_code, id, fecha, det, nombre_docente, nombre_asignatura, result_id, feliz, interesado, motivado, entusiasmado, preocupado, temeroso, triste, cansado, UIDE, numrandom, rn) VALUES (387249, 13722, '2025-01-28 20:54:26', 'null', 'Fredy rolando alvarado gonzalez', 'Los procesos penales y recursos', '13722', '0', '1', '0', '0', '0', '0', '0', '0', 'null', '387249', '2');
+INSERT INTO FinalProyecto.TablasAnalisisJoin (qr_code, id, fecha, det, nombre_docente, nombre_asignatura, result_id, feliz, interesado, motivado, entusiasmado, preocupado, temeroso, triste, cansado, UIDE, numrandom, rn) VALUES (387249, 13721, '2025-01-28 20:54:26', 'null', 'Fredy rolando alvarado gonzalez', 'Los procesos penales y recursos', '13721', '0', '1', '0', '0', '0', '0', '0', '0', 'null', '387249', '3');
+INSERT INTO TablasAnalisisJoin (qr_code, id, fecha, det, nombre_docente, nombre_asignatura, result_id, feliz, interesado, motivado, entusiasmado, preocupado, temeroso, triste, cansado, UIDE, numrandom, rn) VALUES (387249, 13720, '2025-01-28 20:54:26', 'null', 'Fredy rolando alvarado gonzalez', 'Los procesos penales y recursos', '13720', '0', '0', '0', '0', '0', '0', '0', '0', 'null', '387249', '4');
+INSERT INTO TablasAnalisisJoin (qr_code, id, fecha, det, nombre_docente, nombre_asignatura, result_id, feliz, interesado, motivado, entusiasmado, preocupado, temeroso, triste, cansado, UIDE, numrandom, rn) VALUES (387249, 13719, '2025-01-28 20:54:26', 'null', 'Fredy rolando alvarado gonzalez', 'Los procesos penales y recursos', '13719', '0', '0', '0', '0', '1', '0', '0', '0', 'null', '387249', '5');
+INSERT INTO TablasAnalisisJoin (qr_code, id, fecha, det, nombre_docente, nombre_asignatura, result_id, feliz, interesado, motivado, entusiasmado, preocupado, temeroso, triste, cansado, UIDE, numrandom, rn) VALUES (387249, 13718, '2025-01-28 20:54:26', 'null', 'Fredy rolando alvarado gonzalez', 'Los procesos penales y recursos', '13718', '0', '0', '1', '0', '0', '0', '0', '0', 'Muy buena clase', '387249', '6');
